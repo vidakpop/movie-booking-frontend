d711f22481490139afca3282867a91bbaee4fe1d
vidakpop <daviskpop@gmail.com>
2025-03-08 10:08:03 +0300

    Add Cinema model with name, location, and capacity fields

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Cinema(models.Model):
+    name=models.CharField(max_length=300)
+    location=models.CharField(max_length=300)
+    capacity=models.IntegerField()
+    def __str__(self):
+        return self.name
+    
+
