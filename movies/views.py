31cb29760fc1798f7e4d4f760fc7a9f9c79bd3ac
vidakpop <daviskpop@gmail.com>
2025-03-08 12:37:49 +0300

    Fix import statement for models in views.py

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics
-from, models import Movie, Cinema, Booking
+from . models import Movie, Cinema, Booking
 from .serializer import CinemaSerializer, MovieSerializer, BookingSerializer
 
 #List and create Movies
