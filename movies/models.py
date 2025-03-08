f3bb2823c3df0f0bc229758322f0712bf748817a
vidakpop <daviskpop@gmail.com>
2025-03-08 10:22:47 +0300

    Add Movie model with title, description, genre, release date, duration, and poster fields

--- movies/models.py
+++ movies/models.py
@@ -7,4 +7,14 @@ class Cinema(models.Model):
     def __str__(self):
         return self.name
     
+class Movie(models.Model):
+    title=models.CharField(max_length=300)
+    description=models.TextField()
+    genre=models.CharField(max_length=100)
+    release_date=models.DateField()
+    duration=models.IntegerField(help_text='Duration in minutes')
+    poster=models.ImageField(upload_to='posters/',null=True, blank=True)
+    cinemas=models.ManyToManyField(Cinema,related_name='movies')
+    def __str__(self):
+        return self.title
 
