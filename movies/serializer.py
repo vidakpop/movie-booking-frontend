45b94f85be5ada1f65d5cd84d7324f59629b5f7a
vidakpop <daviskpop@gmail.com>
2025-03-08 12:21:01 +0300

    Add BookingSerializer to handle API data representation for Booking model

--- movies/serializer.py
+++ movies/serializer.py
@@ -9,3 +9,8 @@ class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = '__all__'
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = '__all__'
