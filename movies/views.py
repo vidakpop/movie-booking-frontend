c2264f418bbae42e4deee1dd63b776b050f2224c
vidakpop <daviskpop@gmail.com>
2025-03-08 12:37:04 +0300

    Add MovieListCreateView to list and create Movie instances

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
+from, models import Movie, Cinema, Booking
+from .serializer import CinemaSerializer, MovieSerializer, BookingSerializer
 
-# Create your views here.
+#List and create Movies
+class MovieListCreateView(generics.ListCreateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
