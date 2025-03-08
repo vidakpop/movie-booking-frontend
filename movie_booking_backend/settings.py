fda08f82be9003a7a85558eb8a02c2be5517397b
vidakpop <daviskpop@gmail.com>
2025-03-08 12:11:35 +0300

    Add Jazzmin to installed apps for enhanced admin interface

--- movie_booking_backend/settings.py
+++ movie_booking_backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin', # added for jazzmin
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
