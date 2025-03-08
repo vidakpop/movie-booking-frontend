aaec30c5e4de5096d7aaf8dc0e19185e5c3fd72d
vidakpop <daviskpop@gmail.com>
2025-03-08 12:16:51 +0300

    Add Booking model and register in admin interface

--- movies/admin.py
+++ movies/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Cinema, Movie
+from .models import Cinema, Movie, Booking
 
 admin.site.register(Cinema)
 admin.site.register(Movie)
-
+admin.site.register(Booking)
