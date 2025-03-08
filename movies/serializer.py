228cbd7cde02fd04c4c14f40533f9ab9ee5aa1e6
vidakpop <daviskpop@gmail.com>
2025-03-08 12:20:35 +0300

    Add serializer for Movie model to handle API data representation

--- movies/serializer.py
+++ movies/serializer.py
@@ -5,3 +5,7 @@ class CinemaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cinema
         fields = '__all__'
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = '__all__'
