1ea0ee61a9812282303305632663e5372b697519
vidakpop <daviskpop@gmail.com>
2025-03-09 17:37:36 +0300

    Configure JWT token settings for access and refresh token lifetimes

--- movie_booking_backend/settings.py
+++ movie_booking_backend/settings.py
@@ -61,7 +61,13 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
-
+from datetime import timedelta
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
+}
 ROOT_URLCONF = 'movie_booking_backend.urls'
 
 TEMPLATES = [
