f59d6e3cc0e90cb57cb33f6c6bd7dfb4715e3ac7
vidakpop <daviskpop@gmail.com>
2025-03-09 19:08:38 +0300

    Remove SignUpView and LoginView classes from movies/views.py

--- movies/views.py
+++ movies/views.py
@@ -8,29 +8,7 @@ from rest_framework.permissions import AllowAny
 from django.contrib.auth.models import User
 from rest_framework_simplejwt.tokens import RefreshToken
 
-class SignUpview(APIView):
-    permission_classes=[AllowAny]
 
-    def post(self, request):
-        username=request.data.get('username')
-        email=request.data.get('email')
-        password=request.data.get('password')
-
-        if User.objects.filter(username=username).exists():
-            return Response({'error': 'Username already exists'}, status=status.HTTP_400_BAD_REQUEST)
-        
-        user=User.objects.create_user(username=username, email=email,password=password)
-        refresh=RefreshToken.for_user(user)
-
-        return Response({
-            "username": user.username,
-            "access_token": str(refresh.access_token),
-            "refresh_token": str(refresh)
-        })
-class LoginView(APIView):
-    permission_classes=[AllowAny]
-
-    
 
 
 
