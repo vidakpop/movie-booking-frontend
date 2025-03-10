2078e6270317883efae62657d7c9c083c5f04192
vidakpop <daviskpop@gmail.com>
2025-03-10 17:41:55 +0300

    Update MovieListCreateView to require authentication for accessing movie listings

--- movies/views.py
+++ movies/views.py
@@ -4,12 +4,9 @@ from . models import Movie, Cinema, Booking
 from .serializer import CinemaSerializer, MovieSerializer, BookingSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
 from rest_framework_simplejwt.tokens import RefreshToken
-
-
-
 
 
 #List and create Movies
