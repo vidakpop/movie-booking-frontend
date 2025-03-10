5f767eba38fdd6aeab593724cf1fa9d0a38ed442
vidakpop <daviskpop@gmail.com>
2025-03-10 17:39:37 +0300

    Add Booking model and update Movie model with default price

--- movies/models.py
+++ movies/models.py
@@ -15,7 +15,7 @@ class Movie(models.Model):
     release_date=models.DateField()
     duration=models.IntegerField(help_text='Duration in minutes')
     poster=models.ImageField(upload_to='posters/',null=True, blank=True)
-    price=models.DecimalField(max_digits=10,decimal_places=2)
+    price=models.DecimalField(max_digits=10,decimal_places=2,default=0)
     cinemas=models.ManyToManyField(Cinema,related_name='movies')
     def __str__(self):
         return self.title
@@ -25,7 +25,7 @@ class Booking(models.Model):
     movie=models.ForeignKey(Movie,on_delete=models.CASCADE)
     cinema=models.ForeignKey(Cinema,on_delete=models.CASCADE)
     seats=models.IntegerField(default=0)
-    booking_date=models.DateTimeField(auto_now_add=True)
+    booked_at=models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.user.user_name} - {self.movie.title} at {self.cinema.name}"
