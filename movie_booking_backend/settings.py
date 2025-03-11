149cc6464bb0f9581ecd9864efe0688d2b6b4633
vidakpop <daviskpop@gmail.com>
2025-03-11 11:59:12 +0300

    Update timezone setting to Africa/Nairobi and configure media file settings

--- movie_booking_backend/settings.py
+++ movie_booking_backend/settings.py
@@ -125,7 +125,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -136,6 +136,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/posters/'
+MEDIA_ROOT = BASE_DIR / 'posters'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
