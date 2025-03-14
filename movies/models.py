96f091b9aef6960456775c8ecb6c56c95a9d4397
vidakpop <daviskpop@gmail.com>
2025-03-14 10:05:41 +0300

    Add package.json and update dependencies for React Type Animation

--- movies/models.py
+++ movies/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import JSONField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+import json
 
 class Cinema(models.Model):
     name=models.CharField(max_length=300)
