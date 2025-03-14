f84ac77af4bec5b8d358bd36cb41044405c7395b
vidakpop <daviskpop@gmail.com>
2025-03-14 10:55:36 +0300

    imported get object or 404 transaction and isadminuser

--- movies/views.py
+++ movies/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from rest_framework import generics,status
 from . models import Movie, Cinema, Booking
 from .serializer import CinemaSerializer, MovieSerializer, BookingSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated,IsAdminUser
 from django.contrib.auth.models import User
 from rest_framework_simplejwt.tokens import RefreshToken
+from django.db import transaction
 
 
 #List and create Movies
