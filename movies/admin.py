aa38f0cd5fa94a082160ea4a876bb0ec5f29e581
vidakpop <daviskpop@gmail.com>
2025-04-05 10:25:49 +0300

    Register Transaction model in admin for improved management

--- movies/admin.py
+++ movies/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Cinema, Movie, Booking
+from .models import Cinema, Movie, Booking, Transaction
 
 admin.site.register(Cinema)
 admin.site.register(Movie)
 admin.site.register(Booking)
+admin.site.register(Transaction)
